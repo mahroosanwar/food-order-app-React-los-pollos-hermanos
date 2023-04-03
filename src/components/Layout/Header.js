a15c5fda26a95656454cfbb4d70633cac79a46bc
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2023-04-03 16:56:56 +0530

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Los Pollos Hermanos</h1>
+        <HeaderCartButton onClick={props.onShowCart}/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A Table Full of Delicious Food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ src/components/UI/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './Button.module.css';
+
+const Button = props => {
+
+};
+
+export default Button;
